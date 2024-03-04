affc33388c734b5b04bf8ed54645cb6bd76fdb52
Andrew <andrewstaveley14@gmail.com>
2024-03-04 18:52:16 -0500

    Added initial helpers

---
+++ library/helpers.py
@@ -0,0 +1,70 @@
+from models.inventory import Inventory
+from models.master import Master
+from models.store import Store
+
+def exit_program():
+    print("Thank you for using Inventory Monster")
+    exit()
+
+def main_menu():
+    pass
+
+def update_master_list():
+    pass
+
+def add_item_master():
+    pass
+
+def remove_item_master():
+    pass
+
+def add_store():
+    pass
+
+def remove_store():
+    pass
+
+def update_store():
+    pass
+
+def add_stock():
+    pass
+
+def remove_stock():
+    pass
+
+def update_stock():
+    pass
+
+def list_items_master():
+    pass
+
+def list_items_store():
+    pass
+
+def list_stores():
+    pass
+
+def find_store_by_id():
+    pass
+
+def find_store_by_name():
+    pass
+
+def find_item_by_name():
+    pass
+
+def find_item_by_id():
+    pass
+
+def check_store_stock_by_id():
+    pass
+
+def check_store_stock_by_name():
+    pass
+
+def total_inventory_worth():
+    pass
+
+def store_total_inventory_worth():
+    pass
