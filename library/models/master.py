1ba1ff4650e15779ede35dcc643ac6e3d97cf5f9
Andrew <andrewstaveley14@gmail.com>
2024-03-06 17:50:41 -0500

    Added more feautures inside of the master model. More to come

--- library/models/master.py
+++ library/models/master.py
@@ -42,3 +42,99 @@ class Master:
             self._price = price
         else:
             raise ValueError("Price must be a number with two decimals, greater than 0.00")
+        
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS master (
+            id INTEGER PRIMARY KEY,
+            name TEXT
+            location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS master;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        sql = """
+            INSERT INTO master (desc, sku, price)
+            VALUES (?, ?, ?)
+        """
+        CURSOR.execute(sql, (self.desc, self.sku, self.price))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+
+    @classmethod
+    def create(cls, desc, sku, price):
+        master = cls(desc, sku, price)
+        master.save()
+        return master
+    
+    def update(self):
+        sql = """
+            UPDATE master
+            SET desc = ?, sku = ?, price = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.desc, self.sku, self.price))
+        CONN.commit()
+
+    def delete(self):
+        sql = """
+            DELETE FROM master
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id))
+        CONN.commit()
+        del type(self).all[self.id]
+        self.id = None
+
+    @classmethod
+    def instance_from_db(cls, row):
+        master = cls.all.get(row[0])
+        if master:
+            master.desc = row[1]
+            master.sku = row[2]
+            master.price = row[3]
+        else:
+            master = cls(row[1], row[2], row[3])
+            master.id = row[0]
+            cls.all[master.id] = master
+        return master
+    
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT *
+            FROM master
+        """
+        rows = CURSOR.execute(sql).fetchall()
+        return [cls.instance_from_db(row) for row in rows]
+    
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT *
+            FROM master
+            WHERE id = ?
+        """
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+    
+    @classmethod
+    def find_by_desc(cls, desc):
+        sql = """
+            SELECT *
+            FROM master
+            WHERE desc is ?
+        """
+        row = CURSOR.execute(sql, (desc,)).fetchone()
+        return cls.instance_from_db(row) if row else None
